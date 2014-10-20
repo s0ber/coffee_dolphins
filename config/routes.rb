e8af4d7d6c6a2dc9264d14c61c161781a270fdf2
Sergey <sergeyshishkalov@gmail.com>
2014-10-21 03:09:32 +0400

    Login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 require 'routing_filters/helper_params'
 
 Rails.application.routes.draw do
-  get 'users/new'
-
   filter :helper_params
 
   root 'application#root'
+
+  get 'login' => 'sessions#new'
+  get 'logout' => 'sessions#destroy'
 
   resources :positions, only: [:index]
   resources :landings, only: [:index]
   resource :statistics, only: [:show]
   resource :finances, only: [:show]
-
 end
