2731fed378f6592bada7650127849e7450829406
Sergey <sergeyshishkalov@gmail.com>
2014-11-02 16:13:39 +0400

    Add Position model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102082450) do
+ActiveRecord::Schema.define(version: 20141102120959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "positions", force: true do |t|
+    t.string   "title"
+    t.string   "category"
+    t.decimal  "price"
+    t.decimal  "profit"
+    t.integer  "availability_level"
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                        null: false
