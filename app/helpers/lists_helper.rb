1f80462b8edf0f2cd8e589bd7003eae1336d020a
Sergey <sergeyshishkalov@gmail.com>
2014-11-02 22:59:08 +0400

    Improve list helpers

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -2,11 +2,15 @@ module ListsHelper
   def list_item(label, value)
     label = content_tag(:div, label, class: 'panel-list_label')
     value = content_tag(:div, value, class: 'panel-list_value')
-    content_tag(:div, label + value, class: 'panel-list_row')
+    list_row(label + value)
   end
 
   def list_text(value)
     value = content_tag(:p, value, class: 'panel-text')
+    list_row(value)
+  end
+
+  def list_row(value)
     content_tag(:div, value, class: 'panel-list_row')
   end
 end
