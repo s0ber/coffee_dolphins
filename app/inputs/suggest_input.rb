9b43eac92ade966e0507e62469ddbb180800b049
Sergey <sergeyshishkalov@gmail.com>
2014-11-13 03:19:41 +0400

    Preparation for suggest input

---
+++ app/inputs/suggest_input.rb
@@ -0,0 +1,18 @@
+class SuggestInput < SimpleForm::Inputs::CollectionInput
+  wrapper_class 'text_box'
+  js_view 'app#suggest'
+
+  def input
+    @builder.input_field(attribute_name, input_html_options.merge(
+      as: :string,
+      value: '',
+      name: '',
+      data: {
+        object_name: object_name,
+        associated_objects_name: reflection_or_attribute_name,
+        associated_objects: collection
+      }
+    ))
+  end
+end
+
