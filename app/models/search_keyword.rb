f6ce66825dac26243e84f55166e4f53235365335
Sergey <sergeyshishkalov@gmail.com>
2014-12-07 19:59:50 +0400

    Search keywords by search count

--- app/models/search_keyword.rb
+++ app/models/search_keyword.rb
@@ -2,5 +2,5 @@ class SearchKeyword < ActiveRecord::Base
   validates :name, presence: true
   belongs_to :position
 
-  default_scope { order(:created_at) }
+  default_scope { order(search_count: :desc) }
 end
