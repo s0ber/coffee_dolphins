50deebfc46a4299e6750cf201c766a81e9413bb2
Sergey <sergeyshishkalov@gmail.com>
2015-11-10 16:46:40 +0300

    Allow action request to normalize query

--- app/business_actions/action_request.rb
+++ app/business_actions/action_request.rb
@@ -1,13 +1,27 @@
 class ActionRequest
   ALLOWED_TYPES = [:get, :post].freeze
 
-  attr_reader :name, :type, :query
+  attr_reader :runner, :type
 
-  def initialize(name:, type:, query:, child_actions: [])
-    @name, @type, @query = name, type, query
+  def initialize(runner:, type:, query:, child_actions: [])
+    @runner, @type, @query = runner, type, query
 
     unless ALLOWED_TYPES.include?(@type)
       raise ArgumentError, message: "ActionRequest#type should be either :get or :post, but :#{@type} is provided"
     end
+
+    if !runner.is_a?(Class) || !(runner.name =~ /^Actions::/)
+      raise ArgumentError, message: 'ActionRequest#runner should be an Action class'
+    end
+  end
+
+  def query
+    normalized_query
+  end
+
+  private
+
+  def normalized_query
+    @_normalized_query ||= ActionQueryNormalizer.normalize_query(@runner, @query)
   end
 end
--- spec/business_actions/action_request_spec.rb
+++ spec/business_actions/action_request_spec.rb
@@ -1,33 +1,79 @@
 describe ActionRequest do
+  before do
+    module Actions; module Test; class Test; end; end; end
+    module Test; class Test; end; end
+
+    allow(ActionQueryNormalizer).to receive(:normalize_query).and_return({})
+  end
+
+  context 'not given required data' do
+    specify { expect { described_class.new(runner: Actions::Test::Test, type: :get) }.to raise_error(ArgumentError) }
+    specify { expect { described_class.new(runner: Actions::Test::Test, query: {}) }.to raise_error(ArgumentError) }
+    specify { expect { described_class.new(type: :get, query: {}) }.to raise_error(ArgumentError) }
+  end
+
   context 'given required data' do
-    subject { described_class.new(name: 'test#test', type: :get, query: {}) }
-    specify { expect(subject.name).to eq('test#test') }
+    subject { described_class.new(runner: Actions::Test::Test, type: :get, query: {}) }
+
+    specify { expect(subject.runner).to eq(Actions::Test::Test) }
     specify { expect(subject.type).to eq(:get) }
     specify { expect(subject.query).to eq({}) }
 
     context 'given valid type' do
       context 'given get type' do
-        subject { described_class.new(name: 'test#test', type: :get, query: {}) }
+        subject { described_class.new(runner: Actions::Test::Test, type: :get, query: {}) }
         specify { expect(subject.type).to eq(:get) }
       end
 
       context 'given post type' do
-        subject { described_class.new(name: 'test#test', type: :post, query: {}) }
+        subject { described_class.new(runner: Actions::Test::Test, type: :post, query: {}) }
         specify { expect(subject.type).to eq(:post) }
       end
     end
 
     context 'given invalid type' do
       it 'throws argument error' do
-        expect { described_class.new(name: 'test#test',
+        expect { described_class.new(runner: Actions::Test::Test,
                                      type: :update,
                                      query: {}) }.to raise_error(ArgumentError,
                                                                  /ActionRequest#type should be either :get or :post, but :update is provided/)
       end
     end
+
+    context 'given valid action runner' do
+      subject { described_class.new(runner: Actions::Test::Test, type: :get, query: {}) }
+      specify { expect(subject.runner).to eq(Actions::Test::Test) }
+    end
+
+    context 'given invalid action runner' do
+      context 'action runner is not from Actions namespace' do
+        it 'throws argument error' do
+          expect { described_class.new(runner: Test::Test, type: :get, query: {}) }.to raise_error(ArgumentError,
+                                                                                                   /ActionRequest#runner should be an Action class/)
+        end
+      end
+
+      context 'action runner is not a class' do
+        it 'throws argument error' do
+          expect { described_class.new(runner: 'test#test', type: :get, query: {}) }.to raise_error(ArgumentError,
+                                                                                                   /ActionRequest#runner should be an Action class/)
+        end
+      end
+    end
   end
 
-  context 'not given required data' do
-    specify { expect { described_class.new(name: 'test#test', type: :get) }.to raise_error(ArgumentError) }
+  describe '#query' do
+    before do
+      allow(ActionQueryNormalizer).to receive(:normalize_query).and_return(normalized: 'query')
+    end
+
+    subject { described_class.new(runner: Actions::Test::Test, type: :get, query: {dirty: 'query'}) }
+
+    it 'always returns normalized query' do
+      expect(ActionQueryNormalizer).to receive(:normalize_query).with(Actions::Test::Test, dirty: 'query').once
+      subject.query
+      subject.query
+      expect(subject.query).to eq(normalized: 'query')
+    end
   end
 end
