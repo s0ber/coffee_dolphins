56082dd24cbf7b740783472ddc58db8e4b5205e6
Sergey <sergeyshishkalov@gmail.com>
2015-12-15 18:51:26 +0300

    Improve bets list

--- app/decorators/bet_decorator.rb
+++ app/decorators/bet_decorator.rb
@@ -1,3 +1,11 @@
 class BetDecorator < ApplicationDecorator
   decorates_association :bookmaker
+
+  def ammount_rub
+    "#{object.ammount_rub} RUB"
+  end
+
+  def prize
+    "#{object.prize} RUB"
+  end
 end
