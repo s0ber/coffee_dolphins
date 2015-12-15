01b3813d6ce275885e09659db735324a121db7b7
Sergey <sergeyshishkalov@gmail.com>
2015-12-15 19:00:38 +0300

    Check if bookmaker has enough money before creating a bet

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,7 +1,14 @@
 class Bet < ActiveRecord::Base
   belongs_to :fork
   belongs_to :bookmaker
+  before_validation :check_bookmaker_has_enough_money
 
   validates :ammount_rub, :prize, :bookmaker_id, :fork_id, :outcome, presence: true
   default_scope { order(:id) }
+
+  def check_bookmaker_has_enough_money
+    if self.ammount_rub && self.bookmaker.ammount_rub < self.ammount_rub
+      self.errors.add :ammount_rub, :not_enough_bookmaker_money
+    end
+  end
 end
